0f3a4012f49e7c6329476d2bbe3680964c8387a7
Jan <janjus2004@gmail.com>
2024-12-17 21:09:39 +0100

    added loader

--- game-dex/app/pokemon/[id]/page.js
+++ game-dex/app/pokemon/[id]/page.js
@@ -1,16 +1,19 @@
 "use client";
 
-import { useState, useEffect, use } from "react";
+import Loader from "../../components/Loader";
 import PokemonDetails from "../../components/PokemonDetails";
+import { useState, useEffect, use } from "react";
 
 export default function Pokemon({ params }) {
   const unwrappedParams = use(params);
+  const [isLoading, setIsLoading] = useState(false);
   const { id } = unwrappedParams;
 
   const [selectedPokemon, setSelectedPokemon] = useState(null);
 
   useEffect(() => {
     async function fetchPokemonDetails() {
+      setIsLoading(true);
       try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
 
@@ -23,6 +26,8 @@ export default function Pokemon({ params }) {
       } catch (error) {
         console.error("Error fetching Pokémon details:", error);
         setSelectedPokemon(null);
+      } finally {
+        setIsLoading(false);
       }
     }
 
@@ -32,6 +37,7 @@ export default function Pokemon({ params }) {
   return (
     <div className="body">
       <div className="main">
+        {isLoading && <Loader />}
         {selectedPokemon && <PokemonDetails data={selectedPokemon} />}
       </div>
     </div>
